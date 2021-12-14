1dff6a26a89480bd5045737e7e5d980ce7c0c70f
Martin Frauchiger <bulk@frauchi.ch>
2021-12-15 00:47:01 +0100

    first implementation, with debug output, works for size=1

--- visuals.py
+++ visuals.py
@@ -15,8 +15,12 @@ import math
               
 def led_position(strip,color=(90,90,90),position=0,size=1,show=True):
     """turn on LED at desired position"""
-    strip.setPixelColor(position, color)
-    strip.setPixelColor(position+1, color)  # additional line to test commit and push
+    lowIndex = position // 1                # is the index of the lowest LED positon
+    lowStrength = 1 - position % 1          # is the Ratio of the lowest position. Assumed linear behavior. The closer the position to lowIndex the brighter it needs to be.
+    print("[0] lowIndex, [1] lowStrength, [2] position, [3] color".format(lowIndex, lowStrength, position, color))  # debug output
+    strip.setPixelColor(lowIndex, (lowStrength*color[0],lowStrength*color[1],lowStrength*color[2]))    # actually there are no checks implemented, if Index is fine
+    highStrength = 1 - lowStrength
+    strip.setPixelColor(lowIndex+1, (highStrength*color[0],highStrength*color[1],highStrength*color[2]))
     if show==True:                          # usefull if used just for calculation
         strip.show()
         
